06749ed1b152bf47babe1574825f7189a2c339fc
Harry Jordan <harry@inquisitivesoftware.com>
2017-09-16 15:50:54 +0100

    Updated url

--- XCAssetPacker.rb
+++ XCAssetPacker.rb
@@ -1,7 +1,7 @@
 class Xcassetpacker < Formula
   desc "Create an Xcode .xcassets package from a folder of images"
   homepage "https://github.com/inquisitivesoft/XCAssetPacker"
-  url "https://github.com/inquisitiveSoft/XCAssetPacker/releases/download/v1.2/xcassetpacker.1.2.tar.gz"
+  url "https://github.com/inquisitiveSoft/XCAssetPacker/releases/download/1.2/xcassetpacker.1.2.tar.gz"
   sha256 "564e07ab233cf672c88d5a76e8f9a6bddfaf1defce76c17001f6531e48565f40"
 
   bottle :unneeded
